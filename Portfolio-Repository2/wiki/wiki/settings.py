592ca3dacf1ee10ea279398d33c05b297a6d2667
anees-72 <mohsinmohsinali775@gmail.com>
2025-03-24 16:12:16 +0000

    modified settings.py

--- Portfolio-Repository2/wiki/wiki/settings.py
+++ Portfolio-Repository2/wiki/wiki/settings.py
@@ -126,10 +126,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/'public'
-STATICFILES_DIRS = [
-    BASE_DIR / "encyclopedia"/"static"
-]
-
+STATIC_ROOT = BASE_DIR / 'public'
+STATICFILES_DIRS=[BASE_DIR / 'encyclopedia' /'static' ]
