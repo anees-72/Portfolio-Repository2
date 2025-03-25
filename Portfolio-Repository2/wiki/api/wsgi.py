2e63f258ae243d3a3556ba6efdd3bbb4480f80db
anees-72 <mohsinmohsinali775@gmail.com>
2025-03-25 11:49:33 +0000

    made few adjustments and ready for deployment

--- Portfolio-Repository2/wiki/api/wsgi.py
+++ Portfolio-Repository2/wiki/api/wsgi.py
@@ -8,10 +8,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+from serverless_wsgi import handle_request
+
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wiki.settings')
 
 application = get_wsgi_application()
-app=application
+app = handle_request(application)
