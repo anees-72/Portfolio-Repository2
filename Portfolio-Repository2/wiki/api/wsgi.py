95715f2fc058c7d3aa66af1a748c0e2c73c7e3a9
anees-72 <mohsinmohsinali775@gmail.com>
2025-03-26 07:12:11 +0000

    modified wsgi.py and settings.py

--- Portfolio-Repository2/wiki/api/wsgi.py
+++ Portfolio-Repository2/wiki/api/wsgi.py
@@ -8,38 +8,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 import os
 from django.core.wsgi import get_wsgi_application
-from serverless_wsgi import handle_request
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wiki.settings")
 application = get_wsgi_application()
 
-
-import os
-from django.core.wsgi import get_wsgi_application
-from serverless_wsgi import handle_request
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wiki.settings")
-application = get_wsgi_application()
-
-def app(event, context):
-    
-    headers = event.get("headers", {})
-    headers["Host"] = headers.get("Host", "wiki-5b2ct87ed-anees-72s-projects.vercel.app")
-    
-    event = {
-        "path": event.get("path", "/"),
-        "httpMethod": event.get("httpMethod", "GET").upper(),
-        "headers": headers,
-        "queryStringParameters": event.get("queryStringParameters", {})
-    }
-
-    response = handle_request(application, event, context)
-    
-
-    body = response.get("body", b"")
-    if isinstance(body, str):
-        response["body"] = body.encode("utf-8")
-    else:
-        response["body"] = bytes(body)  
-    
-    return response
+app=application
